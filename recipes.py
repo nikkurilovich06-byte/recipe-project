d43dcf9e35a49f142932399c6bb70ca69348f1b5
Kurilovich Nikita <nik.kurilovich.06@gmail.com>
2026-06-04 01:02:14 +0300

    feat: add class Recipe

--- recipes.py
+++ recipes.py
@@ -1,3 +1,5 @@
+import copy
+
 class Ingredient:
     def __init__ (self,name: str, quantity: float, unit: str):
         self.name = name
@@ -25,15 +27,61 @@ class Ingredient:
             return NotImplemented
         return self.name==other.name and self.unit==other.unit
 
+
+
+class Recipe:
+    def __init__(self, title: str, ingredients: list[Ingredient]):
+        self.title = title
+        self.ingredients = ingredients
+    
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        for recipe_ingredient in self.ingredients:
+            if recipe_ingredient==ingredient:
+                recipe_ingredient.quantity +=ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+        return 
+    
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        return ratio>0
+    
+    def scale(self, ratio: float) -> Recipe:
+        new_ingredients = []
+        for ingredient in self.ingredients:
+            new_ingredients.append(Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit))
+        return Recipe(self.title, new_ingredients)
+    
+    def __len__(self) -> int:
+        return len(self.ingredients)
+    
+    def __str__(self) -> str:
+        output = self.title
+        for ingredient in self.ingredients:
+             output += '\n'
+             output += f"{ingredient.name}: {ingredient.quantity} {ingredient.unit}"
+        return output
+
+
+'''
 if __name__ == "__main__":
-    print("Проверка класса Ingredient")
+    muka=Ingredient("Мука",500,"г")
+    eggs = Ingredient("Яйцо", 200, "г")
+    cheese = Ingredient("Сыр", 300, "г")
+    water = Ingredient("Вода", 100, "мл")
+    cheese2 = Ingredient("Сыр", 400, "г")
+    pizza_ingredients = [muka, eggs, cheese]
+    pizza=Recipe("Пицца", pizza_ingredients)
 
-    ingr1 = Ingredient("Мука", 500, "г")
-    print(ingr1)
-    print(repr(ingr1))
+    print(Recipe.is_valid_ratio(-10))
+    print(pizza)
+    pizza.add_ingredient(water)
+    print(pizza)
+    pizza.add_ingredient(cheese2)
+    print(pizza)
 
-    try:
-        ingr1.quantity = -300
-        print(ingr1.quantity)
-    except ValueError as error:
-        print("Ожидаемая ошибка:", error)
+    print(pizza.scale(2))
+    print(len(pizza))
+'''   
+
+
