17af29fc01cdc52975ec53f8cea2eae148df9e12
Фамилия Имя <you@edu.hse.ru>
2026-06-04 18:06:04 +0300

    feat: add class DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -123,3 +123,17 @@ class ShoppingList:
         return new_list
     
 
+class DietaryRecipe(Recipe):
+    def __init__(self,title: str, diet_type: str, ingredients: list[Ingredient]|None=None):
+        if ingredients is None:
+            ingredients = []
+        super().__init__(title, ingredients)
+        self.diet_type=diet_type
+
+    def scale(self, ratio:float) -> DietaryRecipe:
+         recipe=super().scale(ratio)
+         return DietaryRecipe(recipe.title, self.diet_type, recipe.ingredients)
+    
+    def __str__(self) -> str:
+        return f"[{self.diet_type}] "+super().__str__()
+
